31191cc71956f6c26cfc2cfbe12c2a79ba774a7e
Courtney Struelens <courtneystruelens@hotmail.com>
2022-05-20 23:34:26 -0700

    fixed delete bug

--- models/index.js
+++ models/index.js
@@ -4,15 +4,15 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: "category_id",
-});
-
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
   onDelete: "CASCADE",
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
